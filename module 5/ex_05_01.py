c97ebd3052dd49a00b91de720755bbd2405e3fc8
jaden pearsall <90724030+jadenpearsall@users.noreply.github.com>
2021-10-12 16:42:53 -0400

    Add files via upload

---
+++ module 5/ex_05_01.py
@@ -0,0 +1,16 @@
+print("Five point one")
+num = 0
+total = 0.0
+while True:
+  sval = input("Enter a number: ")
+  if sval == "done"
+    break
+try:
+    fval = float(sval)
+except:
+    print('Invalid input')
+    continue
+num = num + 1
+total = total + fval
+print("All done")
+print("total; total, "number of numbers entered: ", "average")
---
+++ module 5/ex_05_02.py
@@ -0,0 +1,10 @@
+num = 0
+total = 0.0
+while True:
+  if sval == 'done'
+  break
+  try:
+    largest - 10
+    smallest -2
+  except:
+      print('Invalid input')
